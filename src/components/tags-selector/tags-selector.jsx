69265d73a01a1f5769002b7c4731ab6065845c97
ferg <me@ferg.in>
2017-07-10 17:47:29 +0600

    ui improvements

--- src/components/tags-selector/tags-selector.jsx
+++ src/components/tags-selector/tags-selector.jsx
@@ -4,15 +4,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-
+  tag: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]).isRequired,
+  multiple: PropTypes.bool,
+  onSelect: PropTypes.func.isRequired,
 };
 
 const defaultProps = {
-
+  multiple: false,
 };
 
 class TagsSelector extends React.PureComponent {
   render() {
+    console.log(this.props);
     return <div>TagsSelector</div>;
   }
 }
